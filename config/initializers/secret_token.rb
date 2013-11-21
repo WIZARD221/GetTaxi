51d3969acb408ab0a4d3435157892fe42b6043ff
Yair <ygraif@gmail.com>
2013-11-21 18:55:15 +0200

    Initial Code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GetTaxi::Application.config.secret_key_base = 'cf70e62e6e43af0e91f0fa9f8a1d7555cb575c7259c4df02e220bfe02ed666fb79b0a9ec521211f70352e6cf74964f571f75127a9dc0cd7dd62f6028100f6a3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GetTaxi::Application.config.session_store :cookie_store, key: '_GetTaxi_session'
